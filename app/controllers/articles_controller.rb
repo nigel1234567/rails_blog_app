7c27a72701c99ff13d5c943847b6f716bdd88d57
Nigel Yeo <nigelyyz@yahoo.com.sg>
2022-05-14 14:21:50 +0800

    Completed Blog App project + added 'Delete Comment' and 'User Authentication'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,54 @@
 class ArticlesController < ApplicationController
+
+  # Authentication
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show] # User needs to be authenticated on every action except index and show
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article # If article saves successfully, action redirects to article's page
+    else
+      render :new, status: :unprocessable_entity # Else render with status code 422 Unprocessible Entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+  include Visible
+
+  has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :title, presence: true # Title value must be present
+  validates :body, presence: true, length: { minimum: 10 } # Body value must be present and at least 10 characters long
+
 end
